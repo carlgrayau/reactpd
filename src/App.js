ab582b1686e5fa6b6014bfa32ab6dd22fbef7802
Carl Gray <carl@firstfnf.com>
2019-03-25 17:03:31 +1100

    clean for exam

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
   render() {
     return (
       <div>
+
         <Router>
           <SearchBar sendData={this.getData}></SearchBar>
           <div><p>{this.state.filteredarray}</p></div>
